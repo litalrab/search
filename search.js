1121ea8ff20d026eab7d09fcd3913da1f7b9ac13
unknown <litalrab@gamil.com>
2018-11-25 18:48:57 +0200

    first commit

---
+++ search.js
@@ -0,0 +1,68 @@
+const path = require('path');
+const fs = require('fs');
+
+function searchFilesInDirectory(dir, ext, filter) {
+    if (!fs.existsSync(dir)) {
+        console.log(`Specified directory: ${dir} does not exist`);
+        return;
+    }
+
+    const files = fs.readdirSync(dir);
+    const found = getFilesInDirectory(dir, '\.' + ext);
+    var fileFound='false';
+
+    found.forEach(file => {
+        const fileContent = fs.readFileSync(file);
+
+        // We want full words, so we use full word boundary in regex.
+        const regex = new RegExp('\\b' + filter + '\\b');
+        if (regex.test(fileContent)) {
+            console.log(`Your word was found in file: ${file}`);
+            fileFound=true;
+        }
+    });
+
+    if(fileFound=='false')
+    {
+        console.log(`No file was found `);
+    }
+}
+
+// Using recursion, we find every file with the desired extention, even if its deeply nested in subfolders.
+function getFilesInDirectory(dir, ext) {
+    if (!fs.existsSync(dir)) {
+        console.log(`Specified directory: ${dir} does not exist`);
+        return;
+    }
+
+    let files = [];
+    fs.readdirSync(dir).forEach(file => {
+        const filePath = path.join(dir, file);
+        const stat = fs.lstatSync(filePath);
+
+        // If we hit a directory, apply our function to that dir. If we hit a file, add it to the array of files.
+        if (stat.isDirectory()) {
+            const nestedFiles = getFilesInDirectory(filePath, ext);
+            files = files.concat(nestedFiles);
+        } else {
+            if (path.extname(file) === ext) {
+                files.push(filePath);
+            }
+        }
+    });
+
+    return files;
+}
+//__filename
+//Running application without any parameter prints a simple help message
+if(process.argv.length<5)
+{
+    console.log("USAGE: node search [EXT] [TEXT]");
+    process.exit(-1);
+        
+
+}
+else{
+    searchFilesInDirectory(process.argv[2], process.argv[3], process.argv[4]);
+
+}
